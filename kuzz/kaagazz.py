834d3f74e5e52237e7ce917d0491572da3312461
Ankur Seth <madhumeya@gmail.com>
2020-09-18 22:48:01 +0530

    CunningPlan

--- kuzz/kaagazz.py
+++ kuzz/kaagazz.py
@@ -3,13 +3,31 @@ import json
 
 class KaagazzApp():
 	def __init__(self):
+		self.SetupMeta()
+		self.SetupBlackadder()
+		pass
+
+	def SetupMeta(self):
 		kaagazzJsonPath = "kzapp/data/json/kaagazz.json"
 		with open(kaagazzJsonPath) as f:
 			self.meta = json.load(f)
 		pass
 
+	def SetupBlackadder(self):
+		blackadderJsonPath = "kzapp/data/json/blackadder.json"
+		with open(blackadderJsonPath) as f:
+			self.blackadder = json.load(f)
+		pass
+
+	def CunningPlan(self):
+		for quote in self.blackadder["quotes"]:
+			text = self.blackadder["quotes"][quote]
+			print(text)
+			pass
+		pass
+
 	def Run(self):
-		print(self)
+		self.CunningPlan()
 		pass
 
 	def __str__(self):
