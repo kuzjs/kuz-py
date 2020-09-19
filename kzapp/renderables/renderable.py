e4879cd1ee0143fdfdf6c39e585864e8cf56d682
Ankur Seth <madhumeya@gmail.com>
2020-09-19 08:39:16 +0530

    Added dummy siteutils.GetEntities()

--- kzapp/renderables/renderable.py
+++ kzapp/renderables/renderable.py
@@ -5,4 +5,10 @@ class Renderable():
 	def __init__(self):
 		pass
 
+	def Next(self):
+		self.next
+
+	def Previous(self):
+		self.previous
+
 
--- kzapp/utils/siteutils.py
+++ kzapp/utils/siteutils.py
@@ -3,31 +3,37 @@ from ..renderables import all as Renderables
 
 
 def SetupNextPrevious(arr):
-	for index in range(0, len(arr)-1):
+	for index in range(0, len(arr)):
 		arr[index].index = index
-		arr[index].next = arr[index + 1] if arr[index + 1] else None
-		arr[index].previous = arr[index - 1] if arr[index - 1] else None
+		arr[index].next = arr[index + 1] if (index + 1) < len(arr) else None
+		arr[index].previous = arr[index - 1] if (index > 1) else None
 
-def GetEntities(site):
+def GetEntities(site, EntityClass):
+	entity = EntityClass()
+	arr = [entity]
+	SetupNextPrevious(arr)
+	return arr
 	pass
 
 def GetAuthors(site):
-	return []
+	return GetEntities(site, Renderables.Author)
 	pass
 
 def GetCategories(site):
-	return []
+	return GetEntities(site, Renderables.Category)
 	pass
 
 def GetTags(site):
-	return []
+	return GetEntities(site, Renderables.Tag)
 	pass
 
 def GetCollections(site):
-	return []
+	return GetEntities(site, Renderables.Collection)
 	pass
 
 def GetPages(site):
 	return []
 	pass
 
+
+		
