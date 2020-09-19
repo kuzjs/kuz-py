0284fbf92d937f286116242c282175d0ae0eb111
Ankur Seth <madhumeya@gmail.com>
2020-09-19 06:46:57 +0530

    Added SetupNextPrevious()

--- kzapp/utils/siteutils.py
+++ kzapp/utils/siteutils.py
@@ -2,6 +2,12 @@ from ..renderables import all as Renderables
 
 
 
+def SetupNextPrevious(arr):
+	for index in range(0, len(arr)-1):
+		arr[index].index = index
+		arr[index].next = arr[index + 1] if arr[index + 1] else None
+		arr[index].previous = arr[index - 1] if arr[index - 1] else None
+
 def GetEntities(site):
 	pass
 
